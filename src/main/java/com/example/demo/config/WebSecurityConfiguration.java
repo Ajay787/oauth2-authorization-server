331396f69f0cf2768525d70c7f03b5dc7096d257
Ajay <ajay.k@molecularconnections.com>
2020-06-10 23:25:36 +0530

    first commit

---
+++ src/main/java/com/example/demo/config/WebSecurityConfiguration.java
@@ -0,0 +1,56 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean(name = "authenticationManager")
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password(passwordEncoder.encode("secret")).roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("secret")).roles("ADMIN");
+    }
+    
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+            .requestMatchers().anyRequest()
+            .and()
+                .authorizeRequests()
+                .antMatchers("/oauth/*","/logout").permitAll().and()
+                .logout()
+    			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+        // @formatter:on
+    }
+    
+    
+	/*
+	 * @Autowired public void configureGlobal(final AuthenticationManagerBuilder
+	 * auth) throws Exception { auth .userDetailsService(customUserDetailsService)
+	 * .passwordEncoder(passwordEncoder); }
+	 */
+    
+    
+    
+    
+}
